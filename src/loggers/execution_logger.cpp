993173ec7871a73c979c7914e557388facbeb3fb
hjhhsy120 <hjhhsy120@163.com>
2019-08-09 19:05:58 -0400

    Turn off loggers

--- src/loggers/execution_logger.cpp
+++ src/loggers/execution_logger.cpp
@@ -8,6 +8,7 @@ std::shared_ptr<spdlog::logger> execution_logger;
 void init_execution_logger() {
   execution_logger = std::make_shared<spdlog::logger>("execution_logger", ::default_sink);
   spdlog::register_logger(execution_logger);
+  execution_logger->set_level(spdlog::level::off);
 }
 
 }  // namespace terrier::execution
